468c3b2c52a5bf0785d3a7c9bb8273bfa2e1fb0a
dt9650329-ops <dt9650329@gmail.com>
2026-07-16 18:50:15 -0500

    Implement custom WebChromeClient in MainActivity
    Override onCreate to set a custom WebChromeClient that handles permission requests.

--- capacitor-app/android/app/src/main/java/com/servialiados/cliente/MainActivity.java
+++ capacitor-app/android/app/src/main/java/com/servialiados/cliente/MainActivity.java
@@ -1,5 +1,21 @@
+
 package com.servialiados.cliente;
 
+import android.os.Bundle;
+import android.webkit.PermissionRequest;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebChromeClient;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        bridge.getWebView().setWebChromeClient(new BridgeWebChromeClient(bridge) {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                runOnUiThread(() -> request.grant(request.getResources()));
+            }
+        });
+    }
+}
